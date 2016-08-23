3ddc4905072b540d85316fac10f31831b554c7b5
cblack7057 <cblack7057@gmail.com>
2016-08-23 14:56:20 -0400

    Minor changes to controller and view.
    Consolidated the times into an array of days which contain an array of times.
    Removed unused code.
    Attempted to display all times saved; looks ugly but it'll do for now til we get a better grasp of things.

--- Frontend/js/app2.js
+++ Frontend/js/app2.js
@@ -6,26 +6,13 @@ angular.module('firstApp2', [])
 	vm.message = 'AvailableTimes array';
 	vm.errorMessage = '';
 	
+	vm.week = [[],[],[],[],[],[]];
 	vm.availableTimes = [];
 	vm.day;
-	// this needs to be consolidated into a single array
-	vm.M = [];
-	vm.T = [];
-	vm.W = [];
-	vm.R = [];
-	vm.F = [];
 	
 	// form data
 	vm.timeData = {};
 	vm.dayData = {};
-	
-	vm.compareTo = function compare(a,b) {
-		if (new Date(a.startTime) < new Date(b.startTime))
-			return -1;
-		if (new Date(a.startTime) < new Date(b.startTime))
-			return 1;
-		return 0;
-	};
 	
 	// Adds start and end times to the selected day's array of available time frames.
 	vm.addTimes = function(){
@@ -33,11 +20,12 @@ angular.module('firstApp2', [])
 			var sTime = new Date(vm.timeData.startTime);
 			var eTime = new Date(vm.timeData.endTime);
 			if(sTime < eTime){
+				sTime = vm.changeTimeFormat(sTime);
+				eTime = vm.changeTimeFormat(eTime);
 				vm.availableTimes.push({
 				startTime: sTime,
 				endTime: eTime
 				});
-				vm.availableTimes.sort(vm.compareTo);
 				vm.timeData = {}; //clears form
 				vm.errorMessage = '';
 			} else {
@@ -46,20 +34,14 @@ angular.module('firstApp2', [])
 		}
 	};
 	
+	vm.changeTimeFormat = function(time){
+		return (time.getHours()<10?'0':'') + time.getHours() + (time.getMinutes()<10?'0':'') + time.getMinutes();
+	};
+	
 	// Changes the displayed table of time frames.	
 	vm.newDay = function(value) {
 		vm.day = value;	
-		if(value == 'Monday'){
-			vm.availableTimes = vm.M;
-		} else if(value == 'Tuesday'){
-			vm.availableTimes = vm.T;
-		} else if(value == 'Wednesday'){
-			vm.availableTimes = vm.W;
-		} else if(value == 'Thursday'){
-			vm.availableTimes = vm.R;
-		} else if (value == 'Friday'){
-			vm.availableTimes = vm.F;
-		}
+		vm.availableTimes = vm.week[value];
 		vm.message = vm.availableTimes;
 	};
 	
