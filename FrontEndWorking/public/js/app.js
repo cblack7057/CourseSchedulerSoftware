ea7f259828c6f9d2412dbda34e149e58a0f320b5
Clifford Black <cblack7057@gmail.com>
2016-11-09 16:53:31 -0500

    Table changes
    - Formatted table to account for resizing.
    - Allows for clickable course buttons that create a modal containing course information. (Needs formatting)

--- FrontEndWorking/public/js/app.js
+++ FrontEndWorking/public/js/app.js
@@ -26,7 +26,7 @@ angular.module('firstApp2', ['scheduleService'])
     vm.dayData = {};
     vm.courseData = {};
     vm.choices = ["Fall2016","Spring2017"];
-	vm.modalCourseData;
+	vm.modalCourseData = null;
     // returned schedule data
     vm.schedules = [];
 	
@@ -110,6 +110,7 @@ angular.module('firstApp2', ['scheduleService'])
                 });
 		vm.times[i] = vm.combine(vm.availableTimes);
 		};
+		vm.semester = 'Fall2016';
 		vm.submitTimes();
 	};
 	vm.addCourses = function () {
@@ -145,7 +146,7 @@ angular.module('firstApp2', ['scheduleService'])
             timesArray: vm.times,
             courseArray: vm.courses
         };
-         vm.message = weekData.term;
+         //vm.message = weekData.term;
         // POST request
         $http({
             method: 'POST',
@@ -235,7 +236,7 @@ angular.module('firstApp2', ['scheduleService'])
                 //vm.message2 = 
             });
         });
-		vm.message = vm.currentSchedule;
+		//vm.message = vm.currentSchedule;
     }
 
     vm.showNextSchedule = function () {
@@ -263,40 +264,5 @@ angular.module('firstApp2', ['scheduleService'])
 			Period: period
 		};
 	};
-
-	vm.setModal = function(courseInfo){
-		vm.modalCourseData = courseInfo;
-	};
-	// modal stuff
-/*
-	
-	// Get the modal
-	var modal = document.getElementById('myModal');
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on the button, open the modal 
-	btn.onclick = function() {
-		modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	}
-	*/
-	
-	
 
 });
