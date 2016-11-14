b5eff2e26daa7267893b5ad51d0d75413d6b8030
NickWeintraut <eldraco@mac.com>
2016-11-14 00:50:12 -0500

    Add copy of current version of mobile app into Demo folder

---
+++ Demo/Mobile/scheduler/www/js/controllers.js
@@ -0,0 +1,262 @@
+angular.module('scheduler.controllers', ['scheduler.services'])
+
+.controller('DashCtrl', function($scope) {})
+.controller('InputScheduleCtrl', function ($scope, $http, scheduleService) {
+
+    var vm = $scope;
+    // Set from form
+    vm.message = 'AvailableTimes array';
+    vm.message2 = 'test';
+    vm.errorMessageTimes = '';
+	vm.errorMessageCourses = '';
+
+    // entire array represents a week, each inner array represents a day,
+    // each day will hold multiple jsons that have a start time and an end time
+    vm.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    vm.times = [[], [], [], [], [], []];
+    for (i = 0; i < vm.times.length; i++) {
+      vm.times[i].push({
+  					dispStartTime: "12:00 AM",
+  					dispEndTime: "11:59 PM",
+                      StartTime: 0000,
+                      EndTime: 2359
+                  });
+    }
+    vm.courses = [];
+
+    vm.availableTimes = []; // array that the user is currently adding time frames to
+    vm.day; // current day selected, used to determine which array to add the start and end times
+
+    // form data
+    vm.timeData = {};
+    vm.dayData = {};
+    vm.courseData = {};
+
+    // returned schedule data
+    vm.schedules = [];
+
+    // test times represents a single schedule
+
+    // current schedule has
+    vm.currentSchedule = [[], [], [], [], [], [], []];
+    vm.currentScheduleIndex = 0; //In the event we wish to locate a specific schedule, we need this
+
+    //add a new time slot with default values
+    vm.addNewTime = function () {
+      var sTime = new Date();
+      var eTime = new Date();
+      vm.availableTimes.push({
+        startTime: sTime,
+        endTime: eTime,
+        dispStartTime: vm.changeTimeFormat(sTime),
+        dispEndTime: vm.changeTimeFormat(eTime)
+      });
+    }
+  vm.slideNames = ["Add Times", "Add Courses"];
+  vm.slideName = vm.slideNames[0];
+  vm.slideHasChanged = function($index){
+    vm.slideName = vm.slideNames[$index];
+  }
+
+  vm.allDay = function() {
+		vm.availableTimes.push({
+					dispStartTime: "12:00 AM",
+					dispEndTime: "11:59 PM",
+                    StartTime: 0000,
+                    EndTime: 2359
+                });
+		vm.times[vm.day] = vm.combine(vm.availableTimes);
+	};
+    vm.addCourse = function() {
+      vm.courses.push({})
+    }
+
+    vm.addCourses = function () {
+        if (vm.courseData.subject != null && vm.courseData.course != null) {
+            var s = vm.courseData.subject;
+            var c = vm.courseData.course;
+            vm.courses.push({
+                subject: s.toUpperCase(),
+                course: c
+            });
+            vm.courseData = {};
+            vm.errorMessageCourses = '';
+        } else {
+            vm.errorMessageCourses = 'Please enter your course subject and course.'
+        }
+        //vm.message = vm.courses;
+    };
+
+    vm.removeTimes = function (index, parentIndex) {
+        vm.times[parentIndex].splice(index, 1);
+    }
+
+    vm.removeCourses = function (index) {
+        vm.courses.splice(index, 1);
+    }
+
+
+    // submits week data to backend through a post request, then sets data the 'message'
+    vm.submitTimes = function () {
+        vm.currentScheduleIndex = 0;
+        var weekData = {
+            timesArray: vm.times,
+            courseArray: vm.courses
+        };
+        scheduleService.onStartProcessing();
+        // vm.message = weekData;
+        // POST request
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/api/process',
+            data: weekData,
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data) {
+            // extract "Schedule" array from the returned data and save
+            vm.schedules = data;
+            vm.message2 = data;
+            //scheduleService.onFinishProcessing(data);
+            //vm.setCurrentSchedule(vm.currentScheduleIndex); // sets current schedule to the first one
+            scheduleService.onFinishProcessing(data);
+            sm.setCurrentSchedule(sm.currentScheduleIndex);
+
+        });
+        //vm.message2 = 'not reached';
+        //vm.setCurrentSchedule(0);
+
+    };
+
+    // changes time format to hhmm to be stored in respective arrays
+    vm.changeTimeFormat = function (time) {
+        return (time.getHours() < 10 ? '0' : '') + time.getHours() + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes();
+    };
+
+    // Changes the displayed table of times frames
+    vm.newDay = function (value) {
+        vm.day = value;
+        vm.availableTimes = vm.times[value];
+        //vm.message = vm.availableTimes;
+    };
+
+
+    // source http://stackoverflow.com/questions/26390938/merge-arrays-with-overlapping-values
+    vm.combine = function (frames) {
+        var result = [];
+
+        // sort the array
+        frames.sort(function (a, b) {
+            a = a.StartTime;
+            b = b.StartTime;
+
+            if (a > b) {
+                return 1;
+            }
+            if (b < a) {
+                return -1;
+            }
+            return 0;
+        });
+
+        frames.forEach(function (r) {
+            if (!result.length || r.StartTime > result[result.length - 1].EndTime)
+                result.push(r);
+            else
+                result[result.length - 1].EndTime = r.EndTime;
+        });
+
+        return result;
+    };
+
+    vm.showNextSchedule = function () {
+       // if (vm.currentScheduleIndex< vm.schedules.length) { //why not if(currentIndex < vm.schedules.len)?
+            vm.currentScheduleIndex++;
+            vm.setCurrentSchedule(vm.currentScheduleIndex);
+        //};
+    //CR
+	};
+    vm.showPrevSchedule = function () {
+        //if (vm.currentScheduleIndex > 0)) {
+            vm.currentScheduleIndex--;
+            vm.setCurrentSchedule(vm.currentScheduleIndex);
+       // };
+    };
+
+})
+.controller('SelectScheduleCtrl', function($scope, scheduleService) {
+  var sm = $scope;
+  sm.schedules;
+  sm.currentScheduleIndex = 0;
+  sm.waiting = true;
+
+
+  //grabs the list of possible schedules from the local schedule storage service
+  sm.grabSchedules = function(){
+    sm.schedules = scheduleService.getSchedules();
+  };
+
+  sm.listeners = scheduleService.listeners;
+
+  scheduleService.addStartListener(function(){
+    sm.waiting = true;
+  });
+
+  //start listening for changes to schedules data
+  scheduleService.addFinishListener(function(){
+    sm.grabSchedules();
+    sm.setCurrentSchedule(sm.currentScheduleIndex);
+    sm.waiting = false;
+  });
+
+  //grabs the a sample schedule list (with only one schedule) from the local schedule storage service
+  sm.grabSampleSchedules = function(){
+    sm.schedules = scheduleService.getSampleSchedules();
+    sm.waiting = false;
+  };
+
+  sm.setCurrentSchedule = function (scheduleIndex) { //schduleIndex = index of schedule you wish to set as current
+        //use this one when taking schedules from
+        sm.currentSchedule = [[], [], [], [], [], [], []];
+        var tempSchedule = sm.schedules[scheduleIndex];
+
+        //var tempSchedule = vm.testTimes; //to test the single schedule we created
+        sm.message = tempSchedule;
+        // I'll deal with sorting by times later
+        // 1. loop through each class in the schedule to be set
+        // [Deleted]
+        // 3. loop through the meeting times of that class IF they exist, if no meeting add to index[0] of currentSchedule
+        // 4. switch/case 'M', 'T', 'W', 'R', 'F', 'S' to add to index of currentSchedules
+		var tSData;
+		var tEData;
+		var meetings;
+		var tempDay;
+        tempSchedule.forEach(function (c) {
+            meetings = c.Meetings;
+            tempDay = 0;
+
+            meetings.forEach(function (t) {
+				tSData = sm.getVisualTime(t.StartTime);
+				tEData = sm.getVisualTime(t.EndTime);
+                sm.currentSchedule[t.Day].push({
+                    Subject: c.Subj + ' ' + c.Crse,
+                    sTime: tSData.Hours + ':' + tSData.Minutes + ' ' + tSData.Period,
+                    eTime: tEData.Hours + ':' + tEData.Minutes + ' ' + tEData.Period,
+                    courseInfo: c
+                });
+                //vm.message2 =
+            });
+        });
+        sm.message = sm.currentSchedule;
+    }
+
+  sm.getVisualTime = function (time){
+		var hours = Math.round(time / 100) % 12;
+		var minutes = (time % 100 == 0 ? '00' : time % 100);
+		var period = (time >= 1200? 'pm' : 'am');
+		return {
+			Hours: hours,
+			Minutes: minutes,
+			Period: period
+		};
+	};
+
+});
---
+++ Demo/Mobile/scheduler/www/js/services.js
@@ -0,0 +1,98 @@
+angular.module('scheduler.services', [])
+
+	.service('scheduleService', function() {
+		var scheduleStorage = {};
+
+		scheduleStorage.startListeners = [];
+		scheduleStorage.finishListeners = [];
+
+		scheduleStorage.notifyStartListeners = function(){
+			angular.forEach(scheduleStorage.startListeners, function(listenerCallback){
+			 	listenerCallback();
+				//console.log('notified');
+			});
+		};
+
+		scheduleStorage.notifyFinishListeners = function() {
+			angular.forEach(scheduleStorage.finishListeners, function(listenerCallback){
+			 	listenerCallback();
+				//console.log('notified');
+			});
+		}
+
+		scheduleStorage.addStartListener = function(callback){
+			scheduleStorage.startListeners.push(callback);
+		};
+
+		scheduleStorage.addFinishListener = function(callback){
+			scheduleStorage.finishListeners.push(callback);
+		};
+
+		//list of possible schedules, this will be filled with server response
+		scheduleStorage.scheduleArray = [];
+
+		//sample schedule list with one schedule - for testing purposes
+		scheduleStorage.sampleScheduleArray = [[ { Subj: 'CS',
+	      Crse: '01200',
+	      Sect: '1',
+	      Meetings: [Object],
+	      Session: 'Day',
+	      CRN: 41164,
+	      Title: 'COMPUTING ENVIRONMENTS',
+	      Prof: 'Hoxworth Ryan',
+	      Campus: 'Main',
+	      Hrs: 3 },
+	    { Subj: 'CS',
+	      Crse: '04114',
+	      Sect: '1',
+	      Meetings: [Object],
+	      Session: 'Day',
+	      CRN: 41093,
+	      Title: 'OBJ-ORIENT PRGRM/DATA ABSTR',
+	      Prof: 'Myers Jack F',
+	      Campus: 'Main',
+	      Hrs: 4 },
+	    { Subj: 'CS',
+	      Crse: '04222',
+	      Sect: '1',
+	      Meetings: [Object],
+	      Session: 'Day',
+	      CRN: 41096,
+	      Title: 'DATA STRUCT/ALGORIM',
+	      Prof: 'Hristescu Gabriela',
+	      Campus: 'Main',
+	      Hrs: 4 },
+	    { Subj: 'CS',
+	      Crse: '06205',
+	      Sect: '1',
+	      Meetings: [Object],
+	      Session: 'Day',
+	      CRN: 41101,
+	      Title: 'COMPUTER ORGANIZATION',
+	      Prof: 'Mansaray Mohamed S',
+	      Campus: 'Main',
+	      Hrs: 3 } ]];
+
+		//called by InputScheduleCtrl submit callback to store the matching schedules retrieved from server
+		scheduleStorage.onFinishProcessing = function(newSchedules){
+			scheduleStorage.scheduleArray = newSchedules;
+			scheduleStorage.notifyFinishListeners();
+		};
+
+		scheduleStorage.onStartProcessing = function(){
+			scheduleStorage.notifyStartListeners();
+		}
+
+		//called by SelectScheduleCtrl to retrieve store schedule list
+		scheduleStorage.getSchedules = function(){
+			return scheduleStorage.scheduleArray;
+		};
+
+		//gets the sample schedule list for testing purposes
+		scheduleStorage.getSampleSchedules = function(){
+			return scheduleStorage.sampleScheduleArray;
+		};
+
+		return scheduleStorage;
+
+	});
